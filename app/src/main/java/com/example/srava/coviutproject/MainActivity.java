d4b14fc3ea8790fbecb644b76feaa8a4d7815a2e
SanchisFloriane <sanchis.floriane@live.fr>
2016-02-11 15:16:57 +0100

    flo

--- app/src/main/java/com/example/srava/coviutproject/MainActivity.java
+++ app/src/main/java/com/example/srava/coviutproject/MainActivity.java
@@ -2,17 +2,43 @@ package com.example.srava.coviutproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+
+    private static final String FLAG_SUCCESS = "success";
+    private static final String FLAG_MESSAGE = "message";
+    private static final String LOGIN_URL = "http://coviut.esy.es/index.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button test = (Button)findViewById(R.id.button_test);
+        test.setOnClickListener(MyListener);
+
     }
+
+    public View.OnClickListener MyListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()){
+
+                case R.id.button_test :
+                    Log.d("Button test", "ok");
+                    break;
+
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
