20b0d932a71a71d2cad8409310b0bdaf109bcf53
Antoine <antoine.mercier74@gmail.com>
2016-03-07 16:04:46 +0100

    Pour Yohan commit special

--- app/src/main/java/com/example/srava/coviutproject/HttpRequestTaskManager.java
+++ app/src/main/java/com/example/srava/coviutproject/HttpRequestTaskManager.java
@@ -19,6 +19,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+import static com.example.srava.coviutproject.OnPostExecuteFunction.OnPostExecuteInscription;
+import static com.example.srava.coviutproject.OnPostExecuteFunction.OnPostExecuteLogin;
+
 /**
  * Created by sanchisf on 28/01/2016.
  */
@@ -32,8 +35,10 @@ public class HttpRequestTaskManager extends AsyncTask<Credential, String, JSONOb
     }
 
     private String action = "noAction";
-    private static final String FLAG_SUCCESS = "success";
+    private static final String FLAG_SUCCESS = "etat";
+    private static final String FLAG_IDENTIFIANT = "identifiant";
     private static final String FLAG_MESSAGE = "message";
+    private static final String FLAG_DATA = "data";
     private static final String LOGIN_URL = "http://coviut.esy.es/index.php";
 
 
@@ -71,7 +76,7 @@ public class HttpRequestTaskManager extends AsyncTask<Credential, String, JSONOb
         }  catch (IOException e) {
             Log.e("IOException", "Error1");
         }  catch(JSONException e){
-            Log.e("JSONException", "Error23");
+            Log.e("JSONException", "Error23"+e.getMessage());
         }  catch (NetworkOnMainThreadException e){
             Log.e("ThreadException", "android > 3.0!!");
         }
@@ -87,28 +92,28 @@ public class HttpRequestTaskManager extends AsyncTask<Credential, String, JSONOb
 
         //oblige de mettre un TryAndCatch pour une conversion de jSON
         try{
-            Log.d("result",result.getString(FLAG_SUCCESS));
-            int loginOK = result.getInt(FLAG_SUCCESS);
+            Log.d("result success",result.getString(FLAG_SUCCESS));
+            Log.d("result identifiant",result.getString(FLAG_IDENTIFIANT));
+            Log.d("result message",result.getString(FLAG_MESSAGE));
+            Log.d("result data",result.getString(FLAG_DATA));
 
+            int etat = result.getInt(FLAG_SUCCESS);
+            String identifiant = result.getString(FLAG_IDENTIFIANT);
+            String message = result.getString(FLAG_MESSAGE);
+            String data = result.getString(FLAG_DATA);
 
             // check if connection status is OK
-            if(loginOK!=0)
-            {
-                Log.d("context", "" + context);
+            if(identifiant.equals("login")){
+                Log.d("if id", "login");
+                OnPostExecuteLogin(etat, message, data, context);
+            }else if(identifiant.equals("inscription")){
+                Log.d("if id", "inscription");
+                OnPostExecuteInscription(etat, message, data, context);
+            }
 
-                Toast.makeText(context, "Connecté", Toast.LENGTH_SHORT).show();
-                Intent FormChoix = new Intent(context, com.example.srava.coviutproject.FormChoix.class);
-                FormChoix.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(FormChoix);
-            }
-            else
-            {
-                Log.d("context", "" + context);
-                Toast.makeText(context, "Échec de la connection,\nmauvais mot de passe ou nom d'utilisateur", Toast.LENGTH_SHORT).show();
-            }
 
         }  catch(JSONException e){
-            Log.e("JSONException", "Error3");
+            Log.e("JSONException", "Error3"+e.getMessage());
         }  catch (NetworkOnMainThreadException e){
             Log.e("ThreadException", "android > 3.0!!");
         }
---
+++ app/src/main/java/com/example/srava/coviutproject/OnPostExecuteFunction.java
@@ -0,0 +1,56 @@
+package com.example.srava.coviutproject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by merciant on 01/03/2016.
+ */
+public final class OnPostExecuteFunction {
+
+    private OnPostExecuteFunction(){};
+
+    public static void OnPostExecuteLogin(Integer etat, String message, String Data,Context context){
+        if(etat == 1)
+        {
+            Log.d("context", "" + context);
+
+            Toast.makeText(context, "Connecté", Toast.LENGTH_LONG).show();
+            Intent FormChoix = new Intent(context, com.example.srava.coviutproject.FormChoix.class);
+            FormChoix.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(FormChoix);
+        }
+        else
+        {
+            Log.d("context", "" + context);
+            Toast.makeText(context, "Échec de la connection,\nmauvais mot de passe ou nom d'utilisateur", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public static void OnPostExecuteInscription(Integer etat, String message, String Data,Context context){
+        if(etat == 1)
+        {
+            Log.d("context", "" + context);
+            Toast.makeText(context, "Vous êtes inscrit!", Toast.LENGTH_LONG).show();
+            Intent FormChoix = new Intent(context, com.example.srava.coviutproject.FormChoix.class);
+            FormChoix.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(FormChoix);
+        }
+        else
+        {
+            Log.d("context", "" + context);
+            Toast.makeText(context, "Échec de l'inscription,\nveuillez corriger vos erreurs", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+
+
+
+
+}
